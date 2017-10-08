c4a653b8e28df063622bffe331c8e6a51b9c414e
ryansperzel <ryansperzel@gmail.com>
2017-10-08 18:53:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet(n) {
+  if (n <= 400) {
+    return 'This one is on me!'
+  } else if (n >= 2000 && n <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (n > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
